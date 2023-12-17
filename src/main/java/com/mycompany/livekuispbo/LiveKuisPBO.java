d486f5783b2eee0120e8cba418557411f8383487
Radhitka <raditfatner47@gmail.com>
2023-12-17 08:47:55 +0700

    feat: first commit

---
+++ src/main/java/com/mycompany/livekuispbo/LiveKuisPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.livekuispbo;
+
+/**
+ *
+ * @author Radit
+ */
+public class LiveKuisPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
